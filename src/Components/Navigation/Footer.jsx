7e994c72becef69ca72ee51896d1c7bb555c5603
GeniePrinie <geniexpn@gmail.com>
2023-10-30 12:55:09 +0100

    Add Footer, Header, Layout

---
+++ src/Components/Navigation/Footer.jsx
@@ -0,0 +1,15 @@
+/**
+ * Footer component serves as the site's footer.
+ * It displays a message indicating that the site is a course project for educational purposes.
+ *
+ * @component
+ * @returns {JSX.Element} The Footer component.
+ */
+export const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 text-center bg-primary text-white ">
+      This site is a Noroff Frontend development course project by Prinyapon
+      Prinyanut, purely for educational purposes, with no real venues.
+    </footer>
+  );
+};
