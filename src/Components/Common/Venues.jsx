5eabcc1d6ab82204a1a39afe16a0500299eb6f1f
GeniePrinie <geniexpn@gmail.com>
2023-10-31 15:36:33 +0100

    Update Venues component: take out useEffect

--- src/Components/Common/Venues.jsx
+++ src/Components/Common/Venues.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-const url = "https://api.noroff.dev/api/v1/holidaze/venues";
-
-export const Venues = ({ cardLimit }) => {
-  const [venues, setVenues] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(url);
-      const json = await response.json();
-
-      setVenues(json);
-    }
-    getData();
-  }, []);
-
-  const filteredVenues = cardLimit ? venues.slice(0, cardLimit) : venues;
+export const Venues = ({ cardLimit, data }) => {
+  const curatedVenues = cardLimit ? data.slice(0, cardLimit) : data;
 
   return (
     <div className="row text-dark">
-      {filteredVenues.map((venue, index) => (
+      {curatedVenues.map((venue, index) => (
         <div className="col-md-6" key={index}>
           <div className="card">
             <img
@@ -36,7 +22,7 @@ export const Venues = ({ cardLimit }) => {
               <p className="fs-5 card-text mb-0">
                 Guest: 1 - {venue.maxGuests} people
               </p>
-              <p className="fs-5 card-text mb-0">Rating: {venue.rating}</p>
+              <p className="fs-5 card-text mb-0">Rating: {venue.rating}/5</p>
               <p className="fs-5 card-text mb-0">Price: ${venue.price}</p>
               <div className="fs-5 mt-3 d-flex align-items-center my-4 text-uppercase">
                 <Link
