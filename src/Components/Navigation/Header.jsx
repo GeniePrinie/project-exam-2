da939a528b244fba7be6dd1cba88ed8754e7fbdb
GeniePrinie <geniexpn@gmail.com>
2023-11-03 13:01:00 +0100

    Add hamburger menu in Header

--- src/Components/Navigation/Header.jsx
+++ src/Components/Navigation/Header.jsx
@@ -1,30 +1,17 @@
 import { Link } from "react-router-dom";
+import { Burger } from "./NavBurger/Burger";
 
 export const Header = ({ isHomepage }) => {
   const headerClass = isHomepage ? "header-home" : "header-dark";
 
   return (
     <header className={headerClass}>
-      <Link to="/" className="text-decoration-none">
-        <div className="logo-background text-center py-4">
+      <div className="logo-background text-center py-4">
+        <Link to="/" className="text-decoration-none">
           <span className="logo text-light">Holidaze</span>
-        </div>
-      </Link>
-      {/* <nav>
-        <Link to="/">Holidaze</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/venues">Venues</Link>
-        <Link to="/managervenue/:id">Manager venue</Link>
-        <Link to="/managervenues">Manager My Venues</Link>
-        <Link to="/managerprofile">Manager profile</Link>
-        <Link to="/managerbookings">Manager Bookings</Link>
-        <Link to="/customervenue/:id">Customer venue</Link>
-        <Link to="/customerprofile">Customer profile</Link>
-        <Link to="/customerbookings">Customer bookings</Link>
-        <Link to="/customerbookingsuccess">Customer booking success</Link>
-        <Link to="/createvenue">Create Venue</Link>
-      </nav> */}
+        </Link>
+      </div>
+      <Burger />
     </header>
   );
 };
